64582ecb663a47cbdb26bdaec757051674dded72
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2022-01-28 17:48:37 +0100

    create route on model

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Comic;
 use Illuminate\Http\Request;
 
 class homeController extends Controller
 {
-    //
+    public function home(){
+        return view('pages.home');
+    }
 }
